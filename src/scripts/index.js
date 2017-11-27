6176b2d0ba965706928158dc562f7c1609206d72
Uladzimir Vysotski <Uladzimir_Vysotski@epam.com>
2017-11-27 12:59:59 +0300

    finish open\close without error handling

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,37 +4,88 @@ const newsApiBaseAddress = "https://newsapi.org";
 function getChannels(selector) {
   fetch(`${newsApiBaseAddress}/v2/sources?apiKey=${newsApiKey}`, {
     method: "get"
-  }).then(function(response) {    
-    response.json().then((data) => {
+  }).then(function(response) {
+    response.json().then(data => {
       let infoContainer = document.querySelector(selector);
 
       data.sources.forEach(element => {
-        var newElement = document.createElement('div');
-        newElement.innerHTML = element.name;
+        let subContainer = document.createElement("div");
+        subContainer.classList.add("item");
+        subContainer.classList.add("channel-item");
+        subContainer.onclick = () => getNews(element.id);
+
+        let channelTitle = document.createElement("p");
+        channelTitle.innerText = element.name;
+
+        let newElement = document.createElement("div");
         newElement.id = element.id;
-        newElement.classList.add('channel-item');
-        infoContainer.appendChild(newElement);
+
+        subContainer.appendChild(channelTitle);
+        subContainer.appendChild(newElement);
+
+        infoContainer.appendChild(subContainer);
       });
     });
   });
 }
 
 function getNews(channelId) {
-  fetch(`${newsApiBaseAddress}/v1/articles?source=${channelId}&apiKey=${newsApiKey}`, {
-    method: "get"
-  }).then(function(response) {    
-    response.json().then((data) => {
-      let currentBlock = document.querySelector(channelId);
+  let currentBlock = document.querySelector(`#${channelId}`);
 
-      data.sources.forEach(element => {
-        var newElement = document.createElement('div');
-        newElement.innerHTML = element.name;
-        newElement.id = element.id;
-        newElement.classList.add('channel-item');
-        currentBlock.appendChild(newElement);
-      });
-    });
-  });
+  fetchNews(currentBlock, channelId);
 }
 
-getChannels('.container')
+function fetchNews(currentBlock, channelId) {
+  if (currentBlock.hasChildNodes()) {
+    while (currentBlock.firstChild) {
+      currentBlock.removeChild(currentBlock.firstChild);
+    }
+  } else {
+    fetch(
+      `${newsApiBaseAddress}/v1/articles?source=${channelId}&apiKey=${
+        newsApiKey
+      }`,
+      {
+        method: "get"
+      }
+    )
+      .then(function(response) {
+        if (response.ok === true) {
+          response.json().then(data => {
+            data.articles.forEach(element => {
+              createNewsBlock(currentBlock, element);
+            });
+          });
+        }
+      })
+      .catch(function(params) {
+        console.log(`Error ${params}`);
+      });
+  }
+}
+
+function createNewsBlock(currentBlock, element) {
+  let newElement = document.createElement("div");
+  newElement.classList.add("item");
+  newElement.classList.add("news-item");
+
+  let title = document.createElement("a");
+  title.innerHTML = element.title;
+  title.href = element.url;
+  title.classList.add("news-title");
+
+  let paragraph = document.createElement("p");
+  paragraph.innerHTML = element.description;
+  paragraph.classList.add("news-paragraph");
+
+  let image = document.createElement("img");
+  image.setAttribute("src", element.urlToImage);
+  image.classList.add("news-image");
+
+  newElement.appendChild(title);
+  newElement.appendChild(paragraph);
+  newElement.appendChild(image);
+  currentBlock.appendChild(newElement);
+}
+
+getChannels(".container");
